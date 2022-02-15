c1b21ddffecf89d23f4b0dfa8fc49330b4879c2d
joungeun <joungeun@naver.com>
2022-02-16 01:57:34 +0900

    백준 10989

--- 2월/둘째주/수 정렬하기 3.py
+++ 2월/둘째주/수 정렬하기 3.py
@@ -8,7 +8,7 @@ count = [0] * 10001
 for _ in range(int(stdin.readline())):
     count[int(stdin.readline())] += 1
 
-for cnt in range(len(count)):
+for cnt in range(1, len(count)):
     if count[cnt] > 0:
         for _ in range(count[cnt]):
             print(cnt)
